2f74a0e09701c1c6e85039288dd3eb30cc3375ac
Nikita Kirin <kirinnikita1406@gmail.com>
2023-03-05 17:23:38 +0500

    [TASK]: add functions for generating photos' data

--- js/main.js
+++ js/main.js
@@ -7,12 +7,11 @@
  * @return {Number}
  */
 function getRandomNumber(min = 0, max = 0) {
-  if (min < 0 || max < 0 || min > max) {
-    return 0;
-  }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
 }
 
 /**
@@ -23,4 +22,73 @@ function getRandomNumber(min = 0, max = 0) {
  */
 function checkStringLength(string = '', maxLength = 0) {
   return string.length <= maxLength;
+}
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const authors = [
+  'Иван',
+  'Мария',
+  'Максим',
+  'Гриша',
+  'Света',
+];
+
+/**
+ * Storages for ids of entities
+ */
+const photoDescriptionsIds = [];
+const commentsIds = [];
+
+
+/**
+ * Get photo's data
+ * @returns {{comments: {name: string, id: Number, avatar: string, message: string}[], description: string, id: Number, url: string, likes: Number}}
+ */
+function getPhotoDescription() {
+  let id = getRandomNumber(1, 25);
+  while (photoDescriptionsIds.includes(id)) {
+    id = getRandomNumber(1, 25);
+  }
+  photoDescriptionsIds.push(id);
+  return {
+    id,
+    url: `img/avatar-${id}.jpg`,
+    description: 'Тестовое описание!',
+    likes: getRandomNumber(15, 200),
+    comments: Array.from({length: getRandomNumber(1, 10)}, getComment),
+  };
+}
+
+/**
+ * Get comment's data
+ * @returns {{name: string, id: Number, avatar: string, message: string}}
+ */
+function getComment() {
+  let id = getRandomNumber(1, 300);
+  while (commentsIds.includes(id)) {
+    id = getRandomNumber(1, 300);
+  }
+  commentsIds.push(id);
+  return {
+    id,
+    avatar: `img/avatar-${getRandomNumber(1, 6)}`,
+    message: messages[getRandomNumber(0, messages.length - 1)],
+    name: authors[getRandomNumber(0, authors.length - 1)],
+  };
+}
+
+/**
+ * Generate photos' data
+ * @returns {{comments: {name: string, id: Number, avatar: string, message: string}[], description: string, id: Number, url: string, likes: Number}[]}
+ */
+function getPhotoDescriptions() {
+  return Array.from({length: 25}, getPhotoDescription);
 }
